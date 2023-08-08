4e487f560217c398c615e9b1ada0bb9f565eb7b8
Hannah Watsky <hannahlily24@gmail.com>
2023-08-08 19:38:37 -0400

    changes to make base project work

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
     <div id="title">
         Welcome to Whiskey Chaser
     </div>
+    <RouterView />
     <a href="https://www.flaticon.com/free-icons/whiskey" title="whiskey icons">Whiskey icons created by monkik - Flaticon</a>
 </template>
 
--- whiskey/whiskey/whiskey/urls.py
+++ whiskey/whiskey/whiskey/urls.py
@@ -7,8 +7,5 @@ from whiskey.obtain_expiring_auth_token import ObtainExpiringAuthToken
 router = routers.DefaultRouter()
 
 urlpatterns = [
-    path('create_account/', views.CreateAccountView.as_view()),
-    path('login/', ObtainExpiringAuthToken.as_view()),
-    path('logout/', views.LogOutView.as_view()),
     path('', include(router.urls)),
 ]
