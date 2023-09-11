e926e16cb452c4a633d8fa4a3a88224c73ddfe28
Hannah Watsky <hannahlily24@gmail.com>
2023-09-10 22:46:54 -0400

    add parse_email.py

---
+++ whiskey/parse_email.py
@@ -0,0 +1,45 @@
+import mailbox
+import quopri
+import re
+import requests
+import json
+from bs4 import BeautifulSoup
+
+
+mbox = mailbox.mbox("/var/spool/mail/hannah")
+last_message = mbox[mbox.keys()[-1]]
+last_message = quopri.decodestring(last_message.as_string()).decode('utf-8')
+urls = re.findall(r'https:\/\/www\.abc\.virginia\.gov\/limited\/allocated_stores_[a-zA-Z0-9_]+\.html', last_message)
+if not len(urls):
+    # This must not be a drop email.
+    exit
+
+stores_json = {
+    'data': []
+}
+
+url = urls[-1]
+response = requests.get(url)
+
+with open('/usr/share/nginx/whiskeychaser.org/html/stores-master.json', 'r') as f:
+    stores_master = json.load(f)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+p_elements = soup.find_all("p")
+for p in p_elements:
+    if 'Drop date: ' in p.get_text():
+        stores_json['dropDate'] = p.get_text().strip()[10:]
+        break
+
+stores = soup.find_all("table")[-1].find_all('td')
+for store in stores:
+    store_id = store.find('span').get_text()
+    store_id = int(re.findall(r'[0-9]+', store_id)[-1])
+    for master_store_data in stores_master['data']:
+        if master_store_data['store_id'] == store_id:
+            stores_json['data'].append(master_store_data)
+            break
+
+with open('/usr/share/nginx/whiskeychaser.org/html/stores.json', 'w') as f:
+    json.dump(stores_json, f)
