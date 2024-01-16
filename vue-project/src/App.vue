6a3fb71b22646c2e3d218885d124497051948258
Hannah Watsky <hannahlily24@gmail.com>
2024-01-15 20:21:49 -0500

    add link to personal website

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -26,7 +26,7 @@ function navigateToLandingPage() {
     <div class="main-container">
         <RouterView />
         <div class="attrib">
-            <p>Whiskey Chaser was created by Hannah Watsky. If you have any issues, please email <a href="mailto:hannah@mail.whiskeychaser.org">hannah@mail.whiskeychaser.org</a>.</p>
+            <p>Whiskey Chaser was created by <a href="https://hannahwatsky.best/">Hannah Watsky</a>. If you have any issues, please email <a href="mailto:hannah@mail.whiskeychaser.org">hannah@mail.whiskeychaser.org</a>.</p>
             <p>Special thanks to Brandon Cross, a true whiskey chaser, who came up with the idea for this site and the name 😊</p>
             <a class="attrib-link" href="https://www.flaticon.com/free-icons/whiskey" title="whiskey icons">Whiskey icons created by monkik - Flaticon</a>
         </div>
